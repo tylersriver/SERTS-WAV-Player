0cae2c09eac093125153d6b1039045af5232d8aa
Sriver <tyler.w.sriver@eagles.oc.edu>
2016-12-12 20:38:46 -0600

    Add VB WinForm Project

---
+++ WinForms/SERTS-MP3-Player/SERTS-MP3-Player/Form1.Designer.vb
@@ -0,0 +1,115 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.playBtn = New System.Windows.Forms.Button()
+        Me.TrackLabel = New System.Windows.Forms.Label()
+        Me.TrackNames = New System.Windows.Forms.ListBox()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label2
+        '
+        Me.Label2.AccessibleName = "currentTrack"
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.White
+        Me.Label2.Location = New System.Drawing.Point(39, 38)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(0, 13)
+        Me.Label2.TabIndex = 1
+        '
+        'playBtn
+        '
+        Me.playBtn.AccessibleName = "playBtn"
+        Me.playBtn.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom), System.Windows.Forms.AnchorStyles)
+        Me.playBtn.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.playBtn.Location = New System.Drawing.Point(132, 277)
+        Me.playBtn.Name = "playBtn"
+        Me.playBtn.Size = New System.Drawing.Size(75, 33)
+        Me.playBtn.TabIndex = 2
+        Me.playBtn.Text = "Play"
+        Me.playBtn.UseVisualStyleBackColor = True
+        '
+        'TrackLabel
+        '
+        Me.TrackLabel.AutoSize = True
+        Me.TrackLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TrackLabel.ForeColor = System.Drawing.Color.White
+        Me.TrackLabel.Location = New System.Drawing.Point(13, 9)
+        Me.TrackLabel.Name = "TrackLabel"
+        Me.TrackLabel.Size = New System.Drawing.Size(76, 13)
+        Me.TrackLabel.TabIndex = 0
+        Me.TrackLabel.Text = "Track Name"
+        '
+        'TrackNames
+        '
+        Me.TrackNames.FormattingEnabled = True
+        Me.TrackNames.Location = New System.Drawing.Point(16, 30)
+        Me.TrackNames.Name = "TrackNames"
+        Me.TrackNames.Size = New System.Drawing.Size(432, 225)
+        Me.TrackNames.TabIndex = 8
+        '
+        'SerialPort1
+        '
+        Me.SerialPort1.PortName = "COM5"
+        '
+        'Button1
+        '
+        Me.Button1.AccessibleName = "pauseBtn"
+        Me.Button1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom), System.Windows.Forms.AnchorStyles)
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(246, 277)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 33)
+        Me.Button1.TabIndex = 9
+        Me.Button1.Text = "Pause"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(460, 322)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TrackNames)
+        Me.Controls.Add(Me.playBtn)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TrackLabel)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label2 As Label
+    Friend WithEvents playBtn As Button
+    Friend WithEvents TrackLabel As Label
+    Friend WithEvents TrackNames As ListBox
+    Friend WithEvents SerialPort1 As IO.Ports.SerialPort
+    Friend WithEvents Button1 As Button
+End Class
---
+++ WinForms/SERTS-MP3-Player/SERTS-MP3-Player/Form1.vb
@@ -0,0 +1,84 @@
+﻿Imports System.Threading
+
+Public Class Form1
+    Public Enum States
+        PlayMusic
+        PauseMusic
+        StopMusic
+    End Enum
+    Dim play As String = "P"
+    Dim pause As String = "S"
+
+    ' This is a definition of a thread. The thread object is Thread_0 and
+    ' it uses the method Thread_0_method() below as its code.
+
+    ' This defines a delegate subroutine that takes a string as its input
+    Delegate Sub ListBoxDelegate(ByVal myStr As String)
+
+    Dim Thread_0 As New Thread(AddressOf Thread_0_method)
+    ' This makes an instance of the delegate that points to the address
+    ' of the InvokeMethod function. When the delegate is invoked it will
+    ' run the InvokeMethod method on the thread that owns the window handle.
+    Dim ListBoxDel As New ListBoxDelegate(AddressOf ListBoxDelMethod)
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Serial Port initialize
+        Try
+            SerialPort1.Open()
+        Catch
+            MessageBox.Show("Failed to open serial port", "Audio Player",
+           MessageBoxButtons.OKCancel, MessageBoxIcon.Asterisk)
+            Process.GetCurrentProcess.Kill()
+            ' Console.WriteLine("Failed to open serial port") this is old code
+        End Try
+        'Thread_0 initialize
+        Thread_0.IsBackground = True
+        Thread_0.Priority = ThreadPriority.Lowest
+        Thread_0.Start()
+    End Sub
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles TrackLabel.Click
+        '  TrackNames.Items.Add("Testing")
+    End Sub
+    Private Sub Thread_0_method()
+        'Thread code goes here
+        Dim str As String
+        Dim c As Integer
+        str = ""
+
+        SerialPort1.Write("R", 0, 1)
+        Do
+            str = SerialPort1.ReadLine()
+            If Not (str = "E") Then TrackNames.Invoke(ListBoxDel, str)
+        Loop Until str = "E"
+
+
+    End Sub
+    Public Sub ListBoxDelMethod(ByVal myStr As String)
+        TrackNames.Items.Add(myStr)
+    End Sub
+    Private Sub TrackNames_SelectedIndexChanged(sender As Object, e As EventArgs) Handles TrackNames.SelectedIndexChanged
+        'Send play command to keil board
+        SerialPort1.Write(play, 0, 1)
+        'Send track name to keil board
+        'SerialPort1.Write(TrackNames.SelectedItem.ToString())
+        SerialPort1.Write(TrackNames.SelectedIndex.ToString())
+    End Sub
+    Private Sub playBtn_Click(sender As Object, e As EventArgs) Handles playBtn.Click
+        Dim str = ""
+        Dim _continue As Boolean = True
+        SerialPort1.Write(play, 0, 1)
+        Do
+            Try
+                str = SerialPort1.ReadLine()
+                If (str = play) Then SerialPort1.Write(TrackNames.SelectedIndex.ToString(), 0, 1)
+            Catch exc As Exception
+            End Try
+        Loop Until str = play
+
+        'Instruct board to pause playback
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        SerialPort1.Write(pause, 0, 1)
+    End Sub
+End Class
