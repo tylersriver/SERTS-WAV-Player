494f5271b29e498d87f48b9ca05904ae653b4540
Preston Kemp <preston@prestonkemp.com>
2016-12-12 21:15:38 -0600

    Added Get Files btn to VB Interface

--- WinForms/SERTS-MP3-Player/SERTS-MP3-Player/Form1.Designer.vb
+++ WinForms/SERTS-MP3-Player/SERTS-MP3-Player/Form1.Designer.vb
@@ -29,7 +29,7 @@ Partial Class Form1
         Me.TrackNames = New System.Windows.Forms.ListBox()
         Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
         Me.Button1 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
+        Me.getFilebtn = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Label2
@@ -90,14 +90,14 @@ Partial Class Form1
         Me.Button1.Text = "Pause"
         Me.Button1.UseVisualStyleBackColor = True
         '
-        'Button2
+        'getFilebtn
         '
-        Me.Button2.Location = New System.Drawing.Point(373, 1)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(75, 23)
-        Me.Button2.TabIndex = 10
-        Me.Button2.Text = "Button2"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.getFilebtn.Location = New System.Drawing.Point(373, 4)
+        Me.getFilebtn.Name = "getFilebtn"
+        Me.getFilebtn.Size = New System.Drawing.Size(75, 23)
+        Me.getFilebtn.TabIndex = 10
+        Me.getFilebtn.Text = "Get Files"
+        Me.getFilebtn.UseVisualStyleBackColor = True
         '
         'Form1
         '
@@ -105,7 +105,7 @@ Partial Class Form1
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer))
         Me.ClientSize = New System.Drawing.Size(460, 322)
-        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.getFilebtn)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.TrackNames)
         Me.Controls.Add(Me.playBtn)
@@ -123,5 +123,5 @@ Partial Class Form1
     Friend WithEvents TrackNames As ListBox
     Friend WithEvents SerialPort1 As IO.Ports.SerialPort
     Friend WithEvents Button1 As Button
-    Friend WithEvents Button2 As Button
+    Friend WithEvents getFilebtn As Button
 End Class
